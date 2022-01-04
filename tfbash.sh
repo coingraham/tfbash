23da4c9fb09c7365dc47c57addf409c5ef95b8f9
Coin Graham <coin.graham@cleardata.com>
2022-01-04 14:59:55 -0500

    adding url for use

--- tfbash.sh
+++ tfbash.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+# Use this with "wget -O - https://raw.githubusercontent.com/coingraham/tfbash/main/tfbash.sh | bash"
+
+# Update to the version of terraform you want
 VERSION=1.1.2
 sudo git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 sudo $HOME/.tfenv/bin/tfenv install $VERSION

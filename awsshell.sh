77737dc458d086e50f7f00c1bae9ac6a8ac5fb30
Coin Graham <coin.graham@cleardata.com>
2022-01-06 16:30:50 -0500

    moving order so terraform is there.

--- awsshell.sh
+++ awsshell.sh
@@ -6,10 +6,10 @@
 VERSION=1.1.2
 sudo git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 
-sudo ln -s ~/.tfenv/bin/* /usr/local/bin
+sudo $HOME/.tfenv/bin/tfenv install $VERSION
+sudo $HOME/.tfenv/bin/tfenv use $VERSION
 
-tfenv install $VERSION
-tfenv use $VERSION
+sudo ln -s ~/.tfenv/bin/* /usr/local/bin
 
 terraform -v
 
